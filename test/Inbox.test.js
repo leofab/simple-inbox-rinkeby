1cfc198e836726ba80593d7dd9a9b3208a4e23e1
Leo Fabricio <leofabr3@gmail.com>
2023-11-04 12:00:27 -0300

    problems with test.js caused by some conflict with old data strctures

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,14 +1,27 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const { Web3 }  = require('web3');
-const web3 = new Web3(ganache.provider());
+const provider = ganache.provider();
+const web3 = new Web3(provider);
+const { interface, bytecode } = require('../compile');
 
-beforeEach(()=> {
+let accounts;
+let inbox;
+
+beforeEach( async ()=> {
     // Get a list of all accounts
-    web3.eth.getAccounts().then(fetchedAccounts => {console.log(fetchedAccounts);
-    });
+    accounts = await web3.eth.getAccounts();
+
+    // Deploys a contract to one of the fetched accounts
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: [] })
+        .send({ from: accounts[0], gas: '1000000' });
+
+    inbox.setProvider(provider);
 });
 
-describe('Inbox', () =>{
-    it('deploys a contract', () => {});
+describe('Inbox', () => {
+    it('deploys a contract', () => {
+        console.log(inbox);
+    });
 });
